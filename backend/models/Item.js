e02740fea5faafecda47be90a3ca205da4446f4e
IT24100418 <IT24100418@my.sliit.lk>
2026-04-26 22:19:30 +0530

    Initial commit

---
+++ backend/models/Item.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Item name is required"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price cannot be negative"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    serialNumber:{type:Number,
+      required:[true,'Serial Number is required'],
+      min:[0,'Serial Number cannot be negative']
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", itemSchema);
